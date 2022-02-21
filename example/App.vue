7b645fafd40b0a1bb6329dc798b2001e60264a3e
zonemeen <miqilin18@gmail.com>
2022-02-21 13:58:28 +0800

    feat: example case

---
+++ example/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <NotionAvatar bgColor="#debaba" :config="config" />
+</template>
+
+<script setup lang="ts">
+import NotionAvatar, { getRandomConfig } from '../src/main'
+const config = getRandomConfig()
+</script>
