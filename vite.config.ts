199020f6dc52393068b7977fd3e97ef852376dff
zonemeen <miqilin18@gmail.com>
2022-04-15 10:56:42 +0800

    refactor: alias config in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
-    alias: [{ find: '@', replacement: '/src' }],
+    alias: {
+      '@': path.join(__dirname, 'src'),
+    },
   },
   build: {
     lib: {
